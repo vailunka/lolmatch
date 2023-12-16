0af99d952c464ee2b33b4f4c8c6500dc22ab1a18
Ville Ailunka <ailunkaville@gmail.com>
2023-12-16 18:00:56 +0200

    figuring out what coming from riot api and creating participants class

--- LolApiClient.py
+++ LolApiClient.py
@@ -1,38 +1,42 @@
 import requests
 from get_api_key import *
+
+
 class LolApiClient():
 
-    def __init__(self, api_key, region) -> None:
+    def __init__(self, api_key, region, summoner_name) -> None:
         self.api_key = api_key
         self.region = region
+        self.summoner_name = summoner_name
 
-    #api_key = ""
-    #testi_region = "euw1"
-    #testi_summoner_name = "TurboGnome"
-
-    
-
-    def build_url(self, summoner_name):
+    def build_url_to_get_summoner_info(self):
         api_url = "https://" + self.region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/"
-        api_url_key = api_url + summoner_name + '?api_key=' + self.api_key
+        api_url_key = api_url + self.summoner_name + '?api_key=' + self.api_key
+        return api_url_key
+    
+    def build_url_to_get_summoner_match_info(self, encrypted_summoner_id):
+        api_url = "https://" + self.region + ".api.riotgames.com/lol/spectator/v4/active-games/by-summoner/"
+        api_url_key = api_url + encrypted_summoner_id + '?api_key=' + self.api_key
         return api_url_key
     
 
-    def get_encrypted_summoner_name(self, summoner_name):
+    def get_summoner_info(self):
         """This function return player info"""
-        r = requests.get(self.build_url(summoner_name))  
+        r = requests.get(self.build_url_to_get_summoner_info())  
         info = r.json()
+        print(info)
         return info["id"], info["accountId"], info["puuid"], info["name"], info["profileIconId"], info["revisionDate"], info["summonerLevel"]
-
-
-    def game_info(self):
-        pass
-
     
+    def get_encrypted_summoner_id(self):
+        """This function return player info"""
+        r = requests.get(self.build_url_to_get_summoner_info())
+        info = r.json()
+        return info["id"]
 
-client = LolApiClient(get_api_key(), "EUW1")
-print(client.get_encrypted_summoner_name("TurboGnome"))
-
-
+    def get_match_info(self):
+        r = requests.get(self.build_url_to_get_summoner_match_info(self.get_encrypted_summoner_id()))
+        info = r.json()
+        print(info)
+        
 
 
--- testi.py
+++ testi.py
@@ -1,1 +1,8 @@
-print("Hello world")
+from lolapiclient import LolApiClient
+from get_api_key import *
+
+name = input("give name: ")
+region = input("give region: ")
+
+client = LolApiClient(get_api_key(), region, name)
+print(client.get_match_info())
